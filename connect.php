47e1e484ff38b58ae07ab6422084a5d35abe2ff9
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2018-11-13 18:00:19 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+	$hostname = "localhost";
+	$username = "root";
+	$password = "";
+	$databasename = "quanlysinhvien";
+
+	$con = mysqli_connect($hostname,$username,$password,$databasename);
+?>
